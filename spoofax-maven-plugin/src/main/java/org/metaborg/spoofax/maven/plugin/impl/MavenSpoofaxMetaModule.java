e2194570df7337bed870229ea339d40685d786c0
Gabriel Konat <gabrielkonat@gmail.com>
2015-08-03 18:11:14 -0700

    Adapt to new core.

--- spoofax-maven-plugin/src/main/java/org/metaborg/spoofax/maven/plugin/impl/MavenSpoofaxMetaModule.java
+++ spoofax-maven-plugin/src/main/java/org/metaborg/spoofax/maven/plugin/impl/MavenSpoofaxMetaModule.java
@@ -1,15 +1,9 @@
 package org.metaborg.spoofax.maven.plugin.impl;
 
 import org.metaborg.spoofax.meta.core.SpoofaxMetaModule;
-import org.metaborg.spoofax.meta.core.ant.AntRunnerService;
-import org.metaborg.spoofax.meta.core.ant.IAntRunnerService;
-
-import com.google.inject.Singleton;
 
 public class MavenSpoofaxMetaModule extends SpoofaxMetaModule {
     @Override protected void configure() {
         super.configure();
-
-        bind(IAntRunnerService.class).to(AntRunnerService.class).in(Singleton.class);
     }
 }
